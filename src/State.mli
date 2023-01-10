9d296ddca13e1c6b725ae740b77a8ffcd6e3993f
dedeoglu <dilarad.dedeoglu@gmail.com>
2023-01-10 21:13:09 +0100

    fin mli

--- src/State.mli
+++ src/State.mli
@@ -1,3 +1,14 @@
 val compare_state : 's -> 's -> int 
 val verif_depot : 's -> bool
-val do_move : 'a -> 's -> 's 
+val do_move : 'a -> 's -> 's
+type dest = Temp | Vide | Top  
+
+type rank = int (* 1 to 13, valet=11, dame=12, roi=13 *)
+type suit = Trefle | Pique | Coeur | Carreau
+type card = rank * suit
+
+type move = card * dest 
+
+val get_move : string -> (int -> card) * dest
+val move_to_str : int * dest -> string
+
